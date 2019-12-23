856e76b75a19d35945c415e7cbc8fc0459eaa7cb
Marlon <marlon.l-huillier@epitech.eu>
2019-12-23 23:07:36 +0800

    Create display number_of_data chart

---
+++ number_of_data.py
@@ -0,0 +1,24 @@
+
+import os
+import variables
+import matplotlib.pyplot as plt
+import numpy as np
+
+def number_of_data():
+    labels = [d for d in os.listdir(variables.train_path) if os.path.isdir(variables.train_path + d)]
+    print(labels)
+    recordings = []
+    for label in labels:
+        data = [f for f in os.listdir(variables.train_path + '/' + label) if f.endswith('.wav')]
+        recordings.append(len(data))
+    plt.figure(figsize=(30,5))
+    index = np.arange(len(labels))
+    plt.bar(index, recordings)
+    plt.title('# of recordings for each command')
+    plt.xlabel('Command name', fontsize=12)
+    plt.ylabel('# of recordings', fontsize=12)
+    plt.xticks(index, labels, fontsize=8, rotation=60)
+    plt.show()
+
+if __name__ == "__main__":
+    number_of_data()
