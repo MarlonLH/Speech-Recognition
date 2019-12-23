e862ebb3960212298b19cf0f0b4816590d84feb8
Marlon <marlon.l-huillier@epitech.eu>
2019-12-24 00:23:10 +0800

    Create an example

---
+++ example.py
@@ -0,0 +1,18 @@
+import random
+from train import train
+from predict import predict
+from keras.models import load_model
+import IPython.display as ipd
+import numpy as np
+
+def solve(x_val, y_val, classes):
+    model = load_model('best_model.hdf5')
+    index = random.randint(0, len(x_val) - 1)
+    samples = x_val[index].ravel()
+    print("Audio:", classes[np.argmax(y_val[index])])
+    ipd.Audio(samples, rate=8000)
+    print("Prediction:", predict(samples, model, classes))
+
+if __name__ == "__main__":
+    x_val, y_val, classes = train()
+    solve(x_val, y_val, classes)
